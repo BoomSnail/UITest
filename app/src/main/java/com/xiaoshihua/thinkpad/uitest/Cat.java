1b23e5f9e9cd4524dda500f78914c3833b496355
BoomSnail <535020670@qq.com>
2017-04-13 13:37:48 +0800

    增加属性

--- app/src/main/java/com/xiaoshihua/thinkpad/uitest/Cat.java
+++ app/src/main/java/com/xiaoshihua/thinkpad/uitest/Cat.java
@@ -6,6 +6,7 @@ package com.xiaoshihua.thinkpad.uitest;
 public class Cat {
     private String name;
     private int age;
+    private int size;
 
     public Cat() {
     }
@@ -13,6 +14,14 @@ public class Cat {
     public Cat(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     public String getName() {
